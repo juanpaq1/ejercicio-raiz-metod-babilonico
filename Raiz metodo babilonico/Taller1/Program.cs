ae2d728a80fa4949077aeb9c8bbc1ef9a31aa7ee
juanpaq1 <163453679+juanpaq1@users.noreply.github.com>
2024-08-13 09:51:27 -0500

    Add files via upload

---
+++ Raiz metodo babilonico/Taller1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ESCRIBA EL NUMERO A BUSCAR LA RAIZ ");
+            double num = double.Parse(Console.ReadLine());
+
+            double x = num / 2.0;
+            double tolerancia = 0.0000001;
+            double new_num = x;
+            int max_iter = 1000;
+
+            for (int i = 0; i < max_iter; i++)
+            {
+                new_num = 0.5 * (x + num / x);
+
+                if (Math.Abs(new_num - x) < tolerancia)
+                {
+                    break;
+                }
+
+                x = new_num; 
+            }
+
+            Console.WriteLine($"LA RAIZ CUADRADA DE {num} USANDO EL METODO BABILONICO ES {x}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unidad1G2/Taller1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            int age;
+            
+            Console.WriteLine("escriba su nombre: ");
+            name = Console.ReadLine();
+            Console.WriteLine($"hola : {name}");
+
+            Console.WriteLine("::::::::::::::::::::::::::");
+
+            Console.WriteLine("escriba su edad: ");
+            age = int.Parse( Console.ReadLine() );
+            Console.WriteLine($"su edad es : {age}");
+           
+
+            if ( age >= 18)
+            {
+                Console.WriteLine($"{name} es mayor de edad");
+
+            }
+            else
+            {
+                Console.WriteLine($"{name} es menor de edad");
+            }
+            Console.ReadKey();
+        }
+    }
+}

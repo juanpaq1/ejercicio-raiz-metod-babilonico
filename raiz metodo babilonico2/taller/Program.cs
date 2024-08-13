1d7d00868124f8cc65995179c4a5281bb0257b60
juanpaq1 <163453679+juanpaq1@users.noreply.github.com>
2024-08-13 15:58:26 -0500

    Add files via upload

---
+++ raiz metodo babilonico2/taller/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 0, Raiz, aux;
+            Console.WriteLine("ESCRIBA EL NUMERO A BUSCAR LA RAIZ");
+            number = double.Parse(Console.ReadLine());
+
+            do
+            {
+                    Console.WriteLine("ERROR, EL NUMERO DEBE SER MAYOR A CERO");
+                    number = double.Parse(Console.ReadLine()) ;
+                
+            }
+            while (number < 0);
+
+            aux = Math.Round(Math.Sqrt(number));
+            
+            Raiz = (number + (aux* aux))/(2*aux);
+            Console.WriteLine($"LA RAIZ CUADRADA DE {number} por el metodo babilonico es: {Raiz}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
